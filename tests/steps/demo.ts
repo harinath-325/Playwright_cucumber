fcc3b45da983e00a0bef93effcdbcf4456324f45
Harinath <harialam25@gmail.com>
2025-03-13 14:05:52 +0530

    newly added changes

--- tests/steps/demo.ts
+++ tests/steps/demo.ts
@@ -1,14 +1,30 @@
 import{Given,When, Then} from "@cucumber/cucumber"
 
+import{chromium, Page, Browser} from "@playwright/test"
+
+let browser: Browser;
+let page: Page;
+
 Given('checking demo', async function () {
-   console.log("checking demo")
+    browser = await chromium.launch({headless: false});
+    page   = await browser.newPage();
+    await page.goto("https://bookcart.azurewebsites.net/")
+
+    console.log("entering the website")
+
   });
 
   When('print meassage', async function () {
-   console.log("message")
+   await page.locator("//span[text() =' Login ']").click();
+   //await page.locator("//textarea[@aria-label ='Search']").fill("make my trip");
+   //await page.keyboard.press("Enter");
+   console.log("Entered playwright in search box")
  });
 
 
   Then('verify', async function () {
-   console.log("verify")
+    const title = await page.title();
+    console.log(title);
+    //await expect(title).toBe("")
+   //console.log("verify")
  });
