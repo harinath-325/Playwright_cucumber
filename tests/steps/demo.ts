7cfe7f1493725b611925e7ffa01601d4a3ad33da
Harinath <harialam25@gmail.com>
2025-03-19 09:30:30 +0530

    added allure report

--- tests/steps/demo.ts
+++ tests/steps/demo.ts
@@ -1,6 +1,7 @@
 import{Given,When, Then} from "@cucumber/cucumber"
 
 import{chromium, Page, Browser} from "@playwright/test"
+//const { allure } = require('allure-playwright');
 
 let browser: Browser;
 let page: Page;
@@ -8,17 +9,18 @@ let page: Page;
 Given('checking demo', async function () {
     browser = await chromium.launch({headless: false});
     page   = await browser.newPage();
-    await page.goto("https://bookcart.azurewebsites.net/")
+    await page.goto("https://google.com/")
 
     console.log("entering the website")
 
   });
 
   When('print meassage', async function () {
-   await page.locator("//span[text() =' Login ']").click();
-   //await page.locator("//textarea[@aria-label ='Search']").fill("make my trip");
-   //await page.keyboard.press("Enter");
-   console.log("Entered playwright in search box")
+   //await page.locator("//span[text() =' Login ']").click();
+   await page.locator("//textarea[@aria-label ='Search']").fill("make my trip");
+   await page.keyboard.press("Enter");
+   console.log("Entered makemy trip in search box")
+   //console.log("Entered playwright in search box")
  });
 
 
